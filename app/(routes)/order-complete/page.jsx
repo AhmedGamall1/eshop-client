bc936a868ea4fedcec78582c3e93cbba62d6ef8a
ahmed <ag3204123@gmail.com>
2024-02-25 03:12:27 +0200

    deploy

--- app/(routes)/order-complete/page.jsx
+++ app/(routes)/order-complete/page.jsx
@@ -2,19 +2,19 @@ import Header from "@/app/components/LandingSection/Header";
 import Checkout from "@/app/components/checkout/Checkout";
 import OrderComplete from "@/app/components/checkout/OrderComplete";
 import Footer from "@/app/components/footer/Footer";
-import UserProtected from "@/app/hooks/userProtected";
+// import UserProtected from "@/app/hooks/userProtected";
 import Heading from "@/app/utils/Heading";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <UserProtected>
-        <Heading title={"Checkout - Eshop"} />
-        <Header isLanding={false} />
-        <OrderComplete />
-        <Footer />
-      </UserProtected>
+      {/* <UserProtected> */}
+      <Heading title={"Checkout - Eshop"} />
+      <Header isLanding={false} />
+      <OrderComplete />
+      <Footer />
+      {/* </UserProtected> */}
     </>
   );
 };
