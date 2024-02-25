661da297f204a50af949366ed8a9367c2c121f98
ahmed <ag3204123@gmail.com>
2024-02-25 03:18:13 +0200

    deploy

--- app/(routes)/checkout/page.jsx
+++ app/(routes)/checkout/page.jsx
@@ -1,19 +1,19 @@
 import Header from "@/app/components/LandingSection/Header";
 import Checkout from "@/app/components/checkout/Checkout";
 import Footer from "@/app/components/footer/Footer";
-import UserProtected from "@/app/hooks/userProtected";
+// import UserProtected from "../../hooks/UserProtected";
 import Heading from "@/app/utils/Heading";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <UserProtected>
-        <Heading title={"Checkout - Eshop"} />
-        <Header isLanding={false} />
-        <Checkout />
-        <Footer />
-      </UserProtected>
+      {/* <UserProtected> */}
+      <Heading title={"Checkout - Eshop"} />
+      <Header isLanding={false} />
+      <Checkout />
+      <Footer />
+      {/* </UserProtected> */}
     </>
   );
 };
--- app/(routes)/order-complete/page.jsx
+++ app/(routes)/order-complete/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/app/components/LandingSection/Header";
 import Checkout from "@/app/components/checkout/Checkout";
 import OrderComplete from "@/app/components/checkout/OrderComplete";
 import Footer from "@/app/components/footer/Footer";
-// import UserProtected from "@/app/hooks/userProtected";
+// import UserProtected from "../../hooks/UserProtected";
 import Heading from "@/app/utils/Heading";
 import React from "react";
 
